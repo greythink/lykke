6cfcd4e65b20fbbf0b7bda12083a7ab6f93d8f4b
Archit Chandra <architchandra@gmail.com>
2021-09-15 21:55:17 +0530

    Move service teasers into a component, improve styling of services section

---
+++ _data/serviceList.js
@@ -0,0 +1,7 @@
+module.exports = [{
+  title: 'Advisory',
+  description: 'Lykke Advisors offers world-class advisory services in the area of sustainable finance, innovation, and healthcare. We help our clients understand how global sustainability megatrends such as food insecurity, increasing carbon emissions, climate change, and healthcare affect their business.',
+},{
+  title: 'Investments',
+  description: 'Lykke Capital actively invests in technology start-ups in India, across sectors from seed to pre-Series A-stages.',
+}];
