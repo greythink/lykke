e54918ded3486cb29dd0294cb05164b6b7554efa
Archit Chandra <architchandra@gmail.com>
2021-09-15 20:22:18 +0530

    Create hero section on homepage - first pass

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.setTemplateFormats('njk');
   eleventyConfig.addWatchTarget('./styles/');
+  eleventyConfig.addPassthroughCopy("img");
 }
