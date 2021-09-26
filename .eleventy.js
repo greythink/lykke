62764b6ceed59a1c73f077d1df6f80e87cb38b4e
Archit Chandra <architchandra@gmail.com>
2021-09-26 15:04:13 +0530

    Add cache busting logic to style and script links

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,14 @@
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.setTemplateFormats('njk');
+  
   eleventyConfig.addWatchTarget('./styles/');
   eleventyConfig.addWatchTarget('./scripts/');
   eleventyConfig.addPassthroughCopy('img');
   eleventyConfig.addPassthroughCopy({ 'scripts': '.' });
   eleventyConfig.addPassthroughCopy('robots.txt');
-}
+
+  eleventyConfig.addFilter('version', function (value) {
+    const now = String(Date.now());
+    return value + '?v' + now;
+  })
+};
