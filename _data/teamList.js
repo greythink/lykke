719c10377e35ad847ed629a5c004861287cd6f51
Archit Chandra <architchandra@gmail.com>
2021-10-12 19:53:09 +0530

    Remove team member dummy entry

--- _data/teamList.js
+++ _data/teamList.js
@@ -3,9 +3,4 @@ module.exports = [{
   description: 'Tityre, tu patulae recubans sub tegmine fagi dolor. Sed haec quis possit intrepidus aestimare tellus. A communi observantia non est recedendum. Vivamus sagittis lacus vel augue laoreet rutrum faucibus. Donec sed odio operae, eu vulputate felis rhoncus. Quae vero auctorem tractata ab fiducia dicuntur.',
   image: './img/Swarandeep_Singh.jpg',
   linkedin: 'https://www.linkedin.com/in/swarandeepkambo/',
-},{
-  title: 'Swarandeep Singh',
-  description: 'Tityre, tu patulae recubans sub tegmine fagi dolor. Sed haec quis possit intrepidus aestimare tellus. A communi observantia non est recedendum. Vivamus sagittis lacus vel augue laoreet rutrum faucibus. Donec sed odio operae, eu vulputate felis rhoncus. Quae vero auctorem tractata ab fiducia dicuntur.',
-  image: './img/Swarandeep_Singh.jpg',
-  linkedin: 'https://www.linkedin.com/in/swarandeepkambo/',
 }]
