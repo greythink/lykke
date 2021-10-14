3ab3d69670397eb628dbf8e284e3391bc3d01157
Archit Chandra <architchandra@gmail.com>
2021-10-14 22:56:20 +0530

    Add vectors and solutions to Healthcare page

---
+++ _data/solutionList.js
@@ -0,0 +1,10 @@
+module.exports = [{
+  title: 'Assessing Value Proposition',
+  description: 'We help avoid costly and avoidable mistakes by doing market opportunity assessment, data landscape review, find right opportunity points and ultimately prevent disappointing sales.',
+},{
+  title: 'Optimize Patient Finding',
+  description: 'We do both qualitative and quantitative market research so you can develop the products and patients can receive the drugs at affordable rates.',
+},{
+  title: 'Forecasting Sales and Volume',
+  description: 'There is no bigger fear than miscalculating or inaccurately calculating your sales. We apply advanced analytics to provide robust framework for structuring the sales sheets for you.',
+}];
---
+++ _data/vectorList.js
@@ -0,0 +1,10 @@
+module.exports = [{
+  title: 'Physician Vector',
+  description: 'We provide end-to-end consulting covering strategy, operations and tactical processes for your day-to-day operations',
+},{
+  title: 'Patient Vector',
+  description: 'Assisting patients and patient advocacy groups so that the much-needed drugs are made available to patients at affordable costs',
+},{
+  title: 'Pharma Vector',
+  description: 'Using AI/ML to support pharma companies’ at every stage of their drug’s commercial journey – from preclinical to post launch',
+}];
