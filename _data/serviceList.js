4efab694535b975b609dd44c01d0871edd1eee58
Archit Chandra <architchandra@gmail.com>
2022-03-19 22:52:03 +0530

    Improve styling of services section on home page

--- _data/serviceList.js
+++ _data/serviceList.js
@@ -3,5 +3,5 @@ module.exports = [{
   description: 'Lykke Advisors offers world-class advisory services in the area of sustainable finance and innovation. We help our clients understand how global sustainability megatrends such as food insecurity, increasing carbon emissions and climate change affect their business.',
 },{
   title: 'Investments',
-  description: 'Lykke Capital actively invests in technology start-ups in India, across sectors from seed to pre-Series A-stages.',
+  description: '<a href="/lykke-capital">Lykke Capital</a> actively invests in technology start-ups in India, across sectors from seed to pre-Series A-stages.',
 }];
