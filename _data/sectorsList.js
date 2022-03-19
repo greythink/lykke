cfe6631e5ab8db7de89d027bfb65a4ee023475bb
Archit Chandra <architchandra@gmail.com>
2022-03-19 15:38:28 +0530

    Improve copy for and add investment thesis section to Lykke Capital page

---
+++ _data/sectorsList.js
@@ -0,0 +1,10 @@
+module.exports = [{
+  title: 'Sectors',
+  description: 'While we are sector-agnostic in general, we like investing in consumer technology, consumer brands, FinTech and SaaS (consumer and enterprise).',
+},{
+  title: 'Stage',
+  description: 'We typically invest at the early-stage, from angel round to pre-Series A. We only co-invest with leading institutional investors and very rarely lead rounds.',
+},{
+  title: 'Cheque Size',
+  description: 'Our average cheque size is between $70k - $150k.',
+}];
