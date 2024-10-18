8ae5a66784ac6295bcbed66e869b68463cdf468b
Archit Chandra <architchandra@gmail.com>
2024-10-19 00:36:31 +0530

    Add newer press releases

--- _data/pressList.js
+++ _data/pressList.js
@@ -1,18 +1,78 @@
 module.exports = [{
+    title: 'Fashion startup LetsDressUp secures ₹11 crore in pre-series A funding',
+    url: 'https://www.cnbctv18.com/business/startup/fashion-startup-letsdressup-secures-11-crore-in-pre-series-a-funding-19436531.htm',
+    publication: 'CNBC-TV18',
+    month: 'July 2024',
+  },{
+    title: 'LetsDressUp Secures INR 11 Cr in Pre-Series A Funding to Revolutionize D2C Fashion',
+    url: 'https://pune.news/business/letsdressup-secures-inr-11-cr-in-pre-series-a-funding-to-revolutionize-d2c-fashion-198632/',
+    publication: 'Pune News',
+    month: 'July 2024',
+  },{
+    title: 'D2C Fashion Startup LetsDressUp Bags Pre Series A Funding From GVFL, Others',
+    url: 'https://inc42.com/buzz/d2c-fashion-startup-letsdressup-bags-pre-series-a-funding-from-gvfl-others/',
+    publication: 'Inc42',
+    month: 'June 2024',
+  },{
+    title: 'Announcing our investment in Zime, AI-first sales content and training',
+    url: 'https://z21.ventures/blog/announcing-our-investment-in-zime-ai-first-sales-content-and-training',
+    publication: 'z21 Ventures',
+    month: 'June 2024',
+  },{
+    title: 'Agritech startup Sorted raises over $5 million as seed funding',
+    url: 'https://www.moneycontrol.com/news/business/agritech-startup-sorted-raises-over-5-million-as-seed-funding-10292241.html',
+    publication: 'Moneycontrol',
+    month: 'March 2023',
+  },{
+    title: 'Milkbasket cofounder\'s Sorted bags $5 million in seed raise',
+    url: 'https://economictimes.indiatimes.com/tech/funding/milkbasket-cofounders-sorted-bags-5-million-in-seed-raise/articleshow/98879581.cms',
+    publication: 'The Economic Times',
+    month: 'March 2023',
+  },{
+    title: 'Agritech startup Sorted raises over $5 million as seed funding',
+    url: 'https://timesofindia.indiatimes.com/business/startups/companies/agritech-startup-sorted-raises-over-5-million-as-seed-funding/articleshow/98904541.cms',
+    publication: 'The Times of India',
+    month: 'March 2023',
+  },{
+    title: 'Sorted raises $5M in an ongoing seed funding round',
+    url: 'https://indianstartupnews.com/news/sorted-raises-5m-in-an-ongoing-seed-funding-round/',
+    publication: 'Indian Startup News',
+    month: 'March 2023',
+  },{
+    title: 'Indian fintech start-up Recur Club secures $30m seed funding',
+    url: 'https://www.fintechfutures.com/2022/04/indian-fintech-start-up-recur-club-secures-30m-seed-funding/',
+    publication: 'Fintech Futures',
+    month: 'April 2022',
+  },{
+    title: '[Funding alert] Recur Club raises $30M in seed round',
+    url: 'https://yourstory.com/2022/04/funding-alert-recur-club-raises-30-million-seed-round',
+    publication: 'YourStory',
+    month: 'April 2022',
+  },{
+    title: 'B2B Financing Platform Recur Club Raises $30M',
+    url: 'https://www.pymnts.com/news/investment-tracker/2022/b2b-financing-platform-recur-club-raises-30m/',
+    publication: 'PYMNTS',
+    month: 'April 2022',
+  },{
+    title: 'Recur Technologies announces $30 million fundraising',
+    url: 'https://economictimes.indiatimes.com/tech/funding/recur-technologies-announces-30-million-fundraising/articleshow/90930416.cms',
+    publication: 'The Economic Times',
+    month: 'April 2022',
+  },{
   title: 'Plume beauty taps professional makeup market with quality products',
   url: 'https://www.livemint.com/brand-post/plume-beauty-taps-professional-makeup-market-with-quality-products-11643370590084.html',
   publication: 'Mint',
-  month: 'Juanuary 2022',
+  month: 'January 2022',
 },{
   title: 'Plume Beauty closes the angel round with INR 2 crore of funding',
   url: 'https://www.hindustantimes.com/brand-post/plume-beauty-closes-the-angel-round-with-inr-2-crore-of-funding-101643631668694.html',
   publication: 'Hindustan Times',
-  month: 'Juanuary 2022',
+  month: 'January 2022',
 },{
   title: 'Plume Beauty Looks at Growth Escalation after an Impressive Angel Round',
   url: 'https://www.business-standard.com/content/specials/plume-beauty-looks-at-growth-escalation-after-an-impressive-angel-round-122013101176_1.html',
   publication: 'Business Standard',
-  month: 'Juanuary 2022',
+  month: 'January 2022',
 },{
   title: '[Funding alert] LetsDressUp raises $300K in pre-Series A from Titan Capital, Sequoia Capital, others',
   url: 'https://yourstory.com/2021/07/funding-alert-letsdressup-pre-series-a-titan-capital-sequoia-capital/amp',
